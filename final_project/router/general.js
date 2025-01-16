71d535e91bfab4ed72f030de14cecc8fce6396d2
Aristeidis Kartsioukas <aristos9989@gmail.com>
2025-01-16 06:58:55 -0500

    Complete Task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,38 +5,44 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books, null, 4));
+public_users.get('/', function (req, res) {
+    return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const book = books[req.params.isbn]
-  return res.send(JSON.stringify(book, null, 4));
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+    const book = books[req.params.isbn]
+    return res.send(JSON.stringify(book, null, 4));
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let booksToShow = {};
+    for (const [key, value] of Object.entries(books)) {
+        if (books[key]["author"] === author) {
+            booksToShow[key]=value;
+        }
+    };
+    return res.send(JSON.stringify(booksToShow, null, 4));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
