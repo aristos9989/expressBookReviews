762d6d9eeb6f50a4aaab8d7e46601b9610a5a574
Aristeidis Kartsioukas <aristos9989@gmail.com>
2025-01-16 08:53:48 -0500

    Complete Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -48,6 +48,13 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(200).json({ message: "Review added successfully" });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const username = req.session.authorization.username;
+    const isdn = req.params.isbn;
+    delete books[isdn]["reviews"][username];
+    return res.status(200).json({ message: "Review deleted successfully" });
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
