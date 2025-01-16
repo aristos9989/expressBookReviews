319b64ef6c2cd391bc498bbd6b2751462b97946b
Aristeidis Kartsioukas <aristos9989@gmail.com>
2025-01-16 09:15:06 -0500

    Change to Promises for Task 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -32,37 +31,73 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-    return res.send(JSON.stringify(books, null, 4));
+    new Promise((resolve, reject) => {
+        resolve(books);
+    })
+        .then(data => {
+            return res.send(JSON.stringify(data, null, 4));
+        })
+        .catch(error => {
+            console.error('Error fetching books:', error);
+            return res.status(500).json({ message: "Error fetching books" })
+        });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-    const book = books[req.params.isbn]
-    return res.send(JSON.stringify(book, null, 4));
+    new Promise((resolve, reject) => {
+        const book = books[req.params.isbn]
+        resolve(book);
+    })
+        .then(data => {
+            return res.send(JSON.stringify(data, null, 4));
+        })
+        .catch(error => {
+            console.error('Error fetching book:', error);
+            return res.status(500).json({ message: "Error fetching book" })
+        });
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    let booksToShow = {};
-    for (const [key, value] of Object.entries(books)) {
-        if (books[key]["author"] === author) {
-            booksToShow[key] = value;
-        }
-    };
-    return res.send(JSON.stringify(booksToShow, null, 4));
+    new Promise((resolve, reject) => {
+        const author = req.params.author;
+        let booksToShow = {};
+        for (const [key, value] of Object.entries(books)) {
+            if (books[key]["author"] === author) {
+                booksToShow[key] = value;
+            }
+        };
+        resolve(booksToShow);
+    })
+        .then(data => {
+            return res.send(JSON.stringify(data, null, 4));
+        })
+        .catch(error => {
+            console.error('Error fetching books:', error);
+            return res.status(500).json({ message: "Error fetching books" })
+        });
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    let booksToShow = {};
-    for (const [key, value] of Object.entries(books)) {
-        if (books[key]["title"] === title) {
-            booksToShow[key] = value;
-        }
-    };
-    return res.send(JSON.stringify(booksToShow, null, 4));
+    new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let booksToShow = {};
+        for (const [key, value] of Object.entries(books)) {
+            if (books[key]["title"] === title) {
+                booksToShow[key] = value;
+            }
+        };
+        resolve(booksToShow);
+    })
+        .then(data => {
+            return res.send(JSON.stringify(data, null, 4));
+        })
+        .catch(error => {
+            console.error('Error fetching books:', error);
+            return res.status(500).json({ message: "Error fetching books" })
+        });
 });
 
 //  Get book review
