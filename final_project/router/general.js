7b6b7e93cb9804f163dba1f97211222193c78367
Aristeidis Kartsioukas <aristos9989@gmail.com>
2025-01-16 07:01:19 -0500

    Complete Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -35,8 +35,14 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const title = req.params.title;
+    let booksToShow = {};
+    for (const [key, value] of Object.entries(books)) {
+        if (books[key]["title"] === title) {
+            booksToShow[key]=value;
+        }
+    };
+    return res.send(JSON.stringify(booksToShow, null, 4));
 });
 
 //  Get book review
